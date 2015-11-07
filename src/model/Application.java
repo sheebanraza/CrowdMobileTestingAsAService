06b4e3b049d805027c172e11eb69d1689a703b7e
sheebanraza <shaikhsheebanz@gmail.com>
2015-11-06 21:42:04 -0800

    added POJOs for Application and ApplicationProvider

---
+++ src/model/Application.java
@@ -0,0 +1,107 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by Sheeban on 11/3/2015.
+ */
+
+@Entity
+public class Application {
+
+    @Id
+    @GeneratedValue
+    private Integer applicationId;
+
+    @Column
+    @OneToOne
+    private Category category;
+
+    @Column
+    private String currentVersion;
+
+    @OneToOne
+    private ApplicationProvider applicationProvider;
+
+    @OneToOne
+    private Status status;
+
+    @Column
+    private Date startDate;
+
+    @Column
+    private Date endDate;
+
+    public Integer getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(Integer applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public String getCurrentVersion() {
+        return currentVersion;
+    }
+
+    public void setCurrentVersion(String currentVersion) {
+        this.currentVersion = currentVersion;
+    }
+
+    public ApplicationProvider getApplicationProvider() {
+        return applicationProvider;
+    }
+
+    public void setApplicationProvider(ApplicationProvider applicationProvider) {
+        this.applicationProvider = applicationProvider;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    private Application() {
+    }
+
+    @Override
+    public String toString() {
+        return "Application{" +
+                "applicationId=" + applicationId +
+                ", category=" + category +
+                ", currentVersion='" + currentVersion + '\'' +
+                ", applicationProvider=" + applicationProvider +
+                ", status=" + status +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                '}';
+    }
+}
---
+++ src/model/ApplicationProvider.java
@@ -0,0 +1,13 @@
+package model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by Sheeban on 11/6/2015.
+ */
+
+
+@Entity
+public class ApplicationProvider {
+
+}
