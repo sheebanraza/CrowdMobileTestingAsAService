2556b4fc2f1de547d85ae45396f8d43d275dc744
sheebanraza <shaikhsheebanz@gmail.com>
2015-11-07 21:05:37 -0800

    added empty controller

---
+++ src/controller/MobileCrowdTestingController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Sheeban on 11/6/2015.
+ */
+
+@Controller
+public class MobileCrowdTestingController {
+
+}
